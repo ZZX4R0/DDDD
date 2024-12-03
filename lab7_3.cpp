c482689784d78f196137230f6f964d8eefd4e7df
Manisorn <manisorn0803@gmail.com>
2024-12-03 17:22:31 +0700

    3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,14 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
-}
+char before(char input){
+    if(input >= 'A' && input <= 'Z'){
+        if (input == 'A'){
+            return 'Z';
+        }else{
+            return input - 1;
+        }
+    }
+    return '0';
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
 }
